d92f87accc38fb536af18196a55f92880c34e8a0
Tamir M <tamirmayer@gmail.com>
2023-02-28 14:47:04 +0200

    login every 6 hours for aeg devices

--- connections/aeg.py
+++ connections/aeg.py
@@ -1,3 +1,4 @@
+import time
 from devices.aeg import AEGOven
 import pyelectroluxconnect
 
@@ -30,20 +31,30 @@ class AEGConnection(Connection):
             customApiKey=None, 
             customApiBrand=None
         )
+        self._last_login = None
         self._ses.login()
 
+    def check_login(self):
+        if self._last_login == None or self._last_login + 6 * 60 * 60 < time.time():
+            self._ses.login()
+            self._last_login = time.time()
+
     def get_all_appliances(self):
+        self.check_login()
         appllist = self._ses.getAppliances()
         print(appllist)
         for appliance in appllist:  
             print(self._ses.getApplianceConnectionState(appliance))
 
     def get_profile_for_appliance(self, appliance):
+        self.check_login()
         print(self._ses.getApplianceProfile(appliance))
 
     def send_command(self, appliance, hacl, value, destination):
+        self.check_login()
         print (f"Sending command {destination+':'+hacl} with value {value} to {appliance}")
         self._ses.setHacl(appliance, hacl, value, destination)
 
     def read_state(self, appliance):
+        self.check_login()
         return self._ses.getApplianceState(appliance, paramName = None, rawOutput = False)

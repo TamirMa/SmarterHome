8b5004e39f7db2541eff6438daf4db70ee27c0d2
Tamir Mayer <tamirmayer@gmail.com>
2023-06-03 17:41:55 +0300

    Classified the BoschDishwasher as DishwasherInterface

--- devices/homeconnect.py
+++ devices/homeconnect.py
@@ -1,10 +1,10 @@
 import time
 from enum import Enum
-from devices.generic import GenericDevice
+from devices.generic import DishwasherInterface, GenericDevice
 from homeconnect.api import HomeConnectAppliance
 
 
-class BoschDishwasher(GenericDevice):
+class BoschDishwasher(GenericDevice, DishwasherInterface):
 
     class PROGRAMS(str, Enum):
         AUTO = "Dishcare.Dishwasher.Program.Auto2"

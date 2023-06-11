c01713b68e5b50596194fc89bfeac7cfa511c841
Tamir M <tamirmayer@gmail.com>
2023-06-11 20:09:18 +0300

    reload the devices every 5 minutes

--- api_server.py
+++ api_server.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+import datetime
 
 from tools.logger import logger
 from devices.manager import DeviceManager
@@ -14,11 +15,15 @@ from server.shabat_router import shabat_router
 from starlette.middleware import Middleware
 from starlette_context import context
 from starlette_context.middleware import RawContextMiddleware
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 
 SERVER_PORT = int(os.getenv("SERVER_PORT", 9000))
 
 middleware = [Middleware(RawContextMiddleware)]
+
+scheduler = AsyncIOScheduler()
+scheduler.start()
 
 app = FastAPI(
     # dependencies=[Depends(get_query_token)]
@@ -42,12 +47,20 @@ app.include_router(
 
 @app.on_event("startup")
 async def startup_event() -> None:
-    print ("Initializing devices")
+    logger.info("Initializing the device manager")
     device_manager = DeviceManager()
-    device_manager.reload_connections()
-    device_manager.reload_devices()
     context.devices = device_manager
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Mayer's family SmartHome server"}
+
+
+def reload_devices():
+    logger.info("Reloading devices")
+    context.devices.reload_connections()
+    context.devices.reload_devices()
+
+# Schedule the job to run every 5 minutes
+scheduler.add_job(reload_devices, 'interval', minutes=5, next_run_time=datetime.datetime.now() + datetime.timedelta(seconds=10))
+

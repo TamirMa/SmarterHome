d95557453784463158b21fa0888256021cc07025
Tamir Mayer <tamirmayer@gmail.com>
2023-07-01 22:31:21 +0300

    fixed some shabat actions

--- shabat/actions.py
+++ shabat/actions.py
@@ -14,8 +14,8 @@ def shabat_entrance():
     actions.change_light_state("WashingMachineLight", LightState.OFF)
 
     actions.change_light_state("HallEntranceLight", LightState.ON)
-    actions.change_light_state("MainBathroomLight", LightState.ON)
     actions.change_light_state("MainHallLight", LightState.ON)
+    actions.change_light_state("MainBathroomLight", LightState.ON)
     actions.change_light_state("BalconyLight", LightState.ON)
     actions.change_light_state("EntranceLight", LightState.ON)
     actions.change_light_state("KitchenMainLight", LightState.ON)
@@ -33,6 +33,7 @@ def prepare_to_dinner():
     """
     Tunr On - Oven
     """
+    actions.change_socket_state("KitchenSocket", SocketState.ON)
     actions.turn_on_oven()    
 
 def shabat_dinner():
@@ -52,11 +53,12 @@ def prepare_to_sleep():
     Turn Off - MasterBedroomLight
     """
     actions.change_light_state("OfficeLight", LightState.OFF)
-    actions.change_light_state("ClosetRoomLight", LightState.OFF)
     actions.change_light_state("MainHallLight", LightState.OFF)
+    actions.change_light_state("ClosetRoomLight", LightState.OFF)
     actions.change_light_state("MasterBedroomLight", LightState.OFF)
-    actions.change_socket_state("KitchenSocket", SocketState.OFF)
     actions.change_fan_state("MasterBedroomFan", FanState.FAN3)
+    
+    actions.change_socket_state("KitchenSocket", SocketState.OFF)
     
 
 def shutdown_livingroom():
@@ -64,8 +66,8 @@ def shutdown_livingroom():
     Turn Off - Balcony, Entrance Light, Kitchen Lights, Living Room Light, Toilet, Hand Wash Sink, Kitchen Socket, DiningTable
     Close living room curtain
     """
-    actions.change_light_state("MainHallLight", LightState.OFF)
     actions.change_light_state("HallEntranceLight", LightState.OFF)
+    actions.change_light_state("MainBathroomLight", LightState.OFF)
     actions.change_light_state("DiningTableLight", LightState.OFF)
     actions.change_light_state("BalconyLight", LightState.OFF)
     actions.change_light_state("EntranceLight", LightState.OFF)
@@ -114,6 +116,7 @@ def shabat_before_exit():
     Turn Off - DiningTable
     """
     actions.change_light_state("BalconyLight", LightState.ON)
+    actions.change_light_state("MainBathroomLight", LightState.ON)
     actions.change_light_state("LivingRoomLight", LightState.ON)
     actions.change_light_state("GuestRestroomsSinkLight", LightState.ON)
     actions.change_light_state("DiningTableLight", LightState.OFF)
@@ -132,8 +135,8 @@ def test_scheduler():
     logger.info("Testing shabat_entrance")
     shabat_entrance()
     time.sleep(3)
-    logger.info("Testing shabat_entrance")
-    shabat_entrance()
+    logger.info("Testing shabat_dinner")
+    shabat_dinner()
     time.sleep(3)
     logger.info("Testing prepare_to_sleep")
     prepare_to_sleep()

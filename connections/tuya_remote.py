dd3f314e7de92fbb548bdc6ce55f0f6c766be6d1
Tamir M <tamirmayer@gmail.com>
2024-11-06 01:24:52 +0200

    Initial support for TuyaRemote

---
+++ connections/tuya_remote.py
@@ -0,0 +1,41 @@
+import time
+import json
+
+from connections.connection import Connection
+from devices.tuya_remote import TuyaRemoteFingerbotDevice, TuyaRemoteFingerbotOvenDevice
+from tuya_connector import TuyaOpenAPI
+
+class TuyaRemoteConnection(Connection):
+
+    NAME = "TuyaRemote"
+
+    DEVICES = {
+        "Fingerbot": TuyaRemoteFingerbotDevice,
+        "FingerbotOven": TuyaRemoteFingerbotOvenDevice,
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(TuyaRemoteConnection, self).__init__(*args, **kwargs)
+        
+        if not (self._connection_params.get("accessId") and self._connection_params.get("accessKey") and self._connection_params.get("endpoint")):
+            raise Exception("Tuya Remote accessId/accessKey/endpoint missing")
+        
+        self._tuya_api = TuyaOpenAPI(
+            self._connection_params.get("endpoint"), 
+            self._connection_params.get("accessId"), 
+            self._connection_params.get("accessKey")
+        )
+        self._tuya_api.connect()
+
+    def send_command(self, device_id, commands):
+        commands = {'commands': commands}
+        self._tuya_api.post(f'/v1.0/iot-03/devices/{device_id}/commands', commands)
+
+    def set_properties(self, device_id, properties_dict, duration=1000):
+        commands = {'properties': json.dumps(properties_dict),"duration":duration}
+        self._tuya_api.post(f'/v2.0/cloud/thing/{device_id}/shadow/properties/desired"', commands)
+        time.sleep(3)
+
+    def get_properties(self, device_id):
+        response = self._tuya_api.get(f'/v2.0/cloud/thing/{device_id}/shadow/properties')
+        return response["result"]["properties"]
